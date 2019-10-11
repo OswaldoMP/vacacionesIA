52fdb7e7e0514b8f77cb5533b9f34cdbc1235f61
OswaldoMP <171152@ids.upchiapas.edu.mx>
2019-10-11 15:52:10 -0500

    respaldo

--- ejemplo/Color.py
+++ ejemplo/Color.py
@@ -15,11 +15,9 @@ cv2.imshow('Principal',imagenPrincipal)
 setGama=input('Gamma :')
 
 imagenPrincipal=imagenPrincipal.astype(float)        
-# gammaY=float(1**float(setGama))#I ~ u^y
-# imagenPrincipal[:,:]=((imagenPrincipal[:,:].astype(float))**(float(setGama)))
+gammaY=float(1**float(setGama))#I ~ u^y
 
-
-# # HDR
+# # visor HDR
 pixelAlto=np.amax(imagenPrincipal)
 print('PixelAlto')
 print(pixelAlto)
@@ -30,7 +28,7 @@ res_debvec = tonemap1.process(imagenPrincipal.copy())#alterando pixel por la gam
 hdrImagen=np.array(hdrImagen,dtype=np.uint8)#refactor a unit8
 cv2.imshow("Gamma "+str(setGama), hdrImagen)  
 
-imagenPrincipal = hdrImagen
+imagenPrincipal = hdrImagen * gammaY #u~i1/y
 
 
 def grayWorld():
@@ -39,12 +37,10 @@ def grayWorld():
     sumaCanalBlue = np.sum(imagenPrincipal[:, :, 0])#obterner suma total, canal azul
     print('.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.')
     print('RGB GW')
-    print(sumaCanalRed,sumaCanalGreen,sumaCanalBlue)
-
-    canal = getCanal(sumaCanalRed,sumaCanalGreen,sumaCanalBlue)#obtener el menor canal para realizar balanceo
-
-    rPrima,gPrima,bPrima = getRgbPrima(canal,sumaCanalRed,sumaCanalGreen,sumaCanalBlue)
-
+    print(sumaCanalRed,sumaCanalGreen,sumaCanalBlue)  
+    minimo = min(sumaCanalBlue,sumaCanalGreen,sumaCanalRed)   
+    print('MINOMOOOO: ',minimo)
+    rPrima,gPrima,bPrima = getRgbPrima(minimo,sumaCanalRed,sumaCanalGreen,sumaCanalBlue)
     imagenPrincipal[:,:,0]=(imagenPrincipal[:,:,0].astype(float))*(float(bPrima))
     imagenPrincipal[:,:,1]=(imagenPrincipal[:,:,1].astype(float))*(float(gPrima))
     imagenPrincipal[:,:,2]=(imagenPrincipal[:,:,2].astype(float))*(float(rPrima))
@@ -60,11 +56,9 @@ def sacaleByMax():
     print('.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.')
     print('RGB SBM')
     print(sumaCanalRed,sumaCanalGreen,sumaCanalBlue)
-
-    canal = getCanal(sumaCanalRed,sumaCanalGreen,sumaCanalBlue)#obtener el menor canal para realizar balanceo
-    
-    rPrima,gPrima,bPrima = getRgbPrima(canal,sumaCanalRed,sumaCanalGreen,sumaCanalBlue)
-
+    minimo = min(sumaCanalBlue,sumaCanalGreen,sumaCanalRed)
+    print('MINOMOOOO: ',minimo)
+    rPrima,gPrima,bPrima = getRgbPrima(minimo,sumaCanalRed,sumaCanalGreen,sumaCanalBlue)
     imagenPrincipal[:,:,0]=(imagenPrincipal[:,:,0].astype(float))*(float(bPrima))
     imagenPrincipal[:,:,1]=(imagenPrincipal[:,:,1].astype(float))*(float(gPrima))
     imagenPrincipal[:,:,2]=(imagenPrincipal[:,:,2].astype(float))*(float(rPrima))
@@ -74,19 +68,17 @@ def sacaleByMax():
     pass
 
 def shadesOfGray():
-    P=6
+    P=float(input('Valor P :'))
+
     sumaCanalRed = np.sum(imagenPrincipal[:, :, 2]**float(P))**float(1/P)
-        # sumaCanalRed = np.sum(imagenPrincipal[:,:,2]**float(P))**float(1/P)
     sumaCanalGreen = np.sum(imagenPrincipal[:, :, 1]**float(P))**float(1/P)
     sumaCanalBlue = np.sum(imagenPrincipal[:, :, 0]**float(P))**float(1/P)
     print('.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.')
     print('RGB SOG')
     print(sumaCanalRed,sumaCanalGreen,sumaCanalBlue)
-
-    canal = getCanal(sumaCanalRed,sumaCanalGreen,sumaCanalBlue)#obtener el menor canal para realizar balanceo
-    # canal=1
-    rPrima,gPrima,bPrima = getRgbPrima(canal,sumaCanalRed,sumaCanalGreen,sumaCanalBlue)
-
+    minimo = min(sumaCanalBlue,sumaCanalGreen,sumaCanalRed)
+    print('MINOMOOOO: ',minimo)    
+    rPrima,gPrima,bPrima = getRgbPrima(minimo,sumaCanalRed,sumaCanalGreen,sumaCanalBlue)
     imagenPrincipal[:,:,0]=(imagenPrincipal[:,:,0].astype(float))*(float(bPrima))
     imagenPrincipal[:,:,1]=(imagenPrincipal[:,:,1].astype(float))*(float(gPrima))
     imagenPrincipal[:,:,2]=(imagenPrincipal[:,:,2].astype(float))*(float(rPrima))
@@ -94,42 +86,12 @@ def shadesOfGray():
     cv2.imshow('shadesOfGray P:'+str(P), cv2.imread('shadesOfGray.jpg'))
     pass
 
-def getCanal( sumaCanalRed,sumaCanalGreen,sumaCanalBlue):
-    # buscar el canal, elegir canal
-    if sumaCanalBlue < sumaCanalGreen: #si blue es menor a green
-        if sumaCanalBlue < sumaCanalRed: #si blue es menor a red
-            canal = 0 #canal es blue
-            print('blue escogido')
-        else: #red fue menor a blue
-            canal = 2
-            print('red escogido')
-    else:#green fue menor a blue
-        if sumaCanalGreen < sumaCanalRed:#si green es menor a red
-            canal = 1
-            print('green escogido')
-        else: #red fue el menor
-            canal = 2
-            print('red escogido')
 
-    print(canal)
-    return canal
-
-
-def getRgbPrima( canal, sumaCanalRed,sumaCanalGreen,sumaCanalBlue):
+def getRgbPrima( minimo, sumaCanalRed,sumaCanalGreen,sumaCanalBlue):
     # realizar operecion por el canal escogido
-    if canal == 0: #canal azul escogido
-        bPrima = 1
-        gPrima = sumaCanalBlue / sumaCanalGreen 
-        rPrima = sumaCanalBlue / sumaCanalRed
-    elif canal == 1: #canal verde escogido
-        gPrima = 1
-        bPrima = sumaCanalGreen / sumaCanalBlue
-        rPrima = sumaCanalGreen / sumaCanalRed
-    elif canal == 2: #canal rojo escogido
-        rPrima = 1
-        gPrima = sumaCanalRed / sumaCanalGreen
-        bPrima = sumaCanalRed / sumaCanalBlue
-
+    bPrima = minimo / sumaCanalBlue
+    gPrima = minimo / sumaCanalGreen 
+    rPrima = minimo / sumaCanalRed
     print('Cambiado')
     print('blue' ,bPrima)
     print('green', gPrima)
--- ejemplo/borde.py
+++ ejemplo/borde.py
@@ -71,16 +71,16 @@ from matplotlib import pyplot as plt
     # plt.show()
 
 #----------------------------------------------------------------------------------
-imagen = cv2.imread('5.png')#obtener la imagen y convertir a gris
-print('RGB')
-colorRed = np.sum(imagen[:, :, 2])
-colorGreen = np.sum(imagen[:, :, 1])
-colorBlue = np.sum(imagen[:, :, 0])
-print(colorRed,colorGreen,colorBlue)
+# imagen = cv2.imread('5.png')#obtener la imagen y convertir a gris
+# print('RGB')
+# colorRed = np.sum(imagen[:, :, 2])
+# colorGreen = np.sum(imagen[:, :, 1])
+# colorBlue = np.sum(imagen[:, :, 0])
+# print(colorRed,colorGreen,colorBlue)
 
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
 # print('tensorflow: %s' % tensorflow.__version__)
@@ -106,3 +106,84 @@ cv2.destroyAllWindows()
 # print(op)
 
 # 
+
+
+
+
+#-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+
+
+
+
+
+# import numpy as np
+# import cv2 
+# import tensorflow
+# import keras
+# from matplotlib import pyplot as plt
+
+#Cargar la mascara
+imagen = cv2.imread('test.jpg',0)#obtener la imagen y convertir a gris
+
+# fijamos el umbral a 127 para obtener una imagen binaria
+# BINARY
+op1,tManual = cv2.threshold(imagen,127,255,cv2.THRESH_BINARY)
+
+# pasamos la imagen,no defimos el umbral, maximo,binarizar con el metodo de otsu
+# OTSU
+op,tOtsu = cv2.threshold(imagen,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)# op = obtener el umbral de otsu. 
+
+# 
+# Local adaptativa
+img = cv2.medianBlur(imagen,5)
+# img = cv2.medianBlur(tOtsu,5)
+tAdta = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                            cv2.THRESH_BINARY,11,2)
+
+# blur = cv2.GaussianBlur(imagen,(5,5),0)
+# blur = cv2.GaussianBlur(tOtsu,(5,5),0)
+# op2,tAdta = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+# obtenemos el histograma de la imagen en tonos de grises
+histograma = cv2.calcHist([imagen],[0],None,[255],[0,255])
+
+
+print('OTSU RES')
+print(op)
+
+print('MANUAL RES')
+print(op1)
+
+plt.plot(histograma)
+plt.axvline(op, color='R', linestyle='dashed', linewidth=1)
+plt.ylabel('Pixel')
+plt.xlabel('Nivel de Gris')
+# plt.title('Histograma ')
+plt.title('Histograma Imagen Original \n Threshold = ' + str(op))
+#Crear un kernel de '1' de 3x3
+# kernel = np.ones((3,3),np.uint8)
+
+#Se aplica la transformacion: Morphological Gradient
+# transformacion = cv2.dilate(imagen,kernel,iterations = 1) - cv2.erode(imagen,kernel,iterations = 1)
+
+#Mostrar el resultado y salir
+#cv2.imshow('resultado',transformacion)
+cv2.imwrite('resultadoBinarizacion/OTSU.jpg',tOtsu)
+cv2.imwrite('resultadoBinarizacion/BINARY.jpg',tManual)
+cv2.imwrite('resultadoBinarizacion/ADATATIVE.jpg',tAdta)
+# cv2.imshow('gray-world', cv2.imread('gray-world.jpg'))
+
+cv2.imshow('ORIGIN',imagen)
+cv2.imshow('OTSU',tOtsu)
+cv2.imshow('BINARY',tManual)  
+cv2.imshow("ADATATIVE", tAdta)      
+plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# print('tensorflow: %s' % tensorflow.__version__)
+# # keras
+
+# print('keras: %s' % keras.__version__)
